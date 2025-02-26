c4f648cd3fac6d77d5f5b9743621f9530eaa1822
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-26 11:22:34 -0600

    completes lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -25,13 +25,31 @@ function QuestionItem({ question, setList }) {
       .catch((error) => console.error('Error deleting question:', error))
   }
 
+  function handleChange(event){
+    const value = parseInt(event.target.value)
+    fetch (`http://localhost:4000/questions/${id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "correctIndex": value
+      })
+    })
+    
+    setList( (prevList) => 
+      prevList.map((question) =>
+      question.id === id ? {...question, correctIndex: value} : question
+    ))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
       <button onClick={handleClick}>Delete Question</button>
     </li>
