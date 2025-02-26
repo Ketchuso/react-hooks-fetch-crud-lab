7fd0394fb142a7e269ceb69c0f3a2a60530b211d
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-26 09:32:40 -0600

    adds the questionList functionality and adds setList to app.js for both questionList and QuestionForm to use

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [list, setList] = useState([]);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList list={list} setList={setList}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ list, setList }) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(resp => resp.json())
+      .then(data => setList(data))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{list.map((items) => {
+        return (
+        <QuestionItem 
+        key={items.id} 
+        question={items}
+        />
+      );
+      })}</ul>
     </section>
   );
 }
